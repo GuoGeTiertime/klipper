6f0b9d43e0ede5d5e843e55c9ac5b2b11feab2f8
TESD\zhaih <zhaihq@tiertime.net>
2025-12-09 16:00:08 +0800

    增加Klipper版本号查询，硬编码设置版本号为2.0.0

--- klippy/gcode.py
+++ klippy/gcode.py
@@ -362,7 +362,7 @@ class GCodeDispatch:
         self.printer.invoke_shutdown("Shutdown due to M112 command")
     def cmd_M115(self, gcmd):
         # Get Firmware Version and Capabilities
-        software_version = self.printer.get_start_args().get('software_version')
+        software_version = 2.0.0
         kw = {"FIRMWARE_NAME": "Klipper", "FIRMWARE_VERSION": software_version}
         msg = " ".join(["%s:%s" % (k, v) for k, v in kw.items()])
         did_ack = gcmd.ack(msg)
